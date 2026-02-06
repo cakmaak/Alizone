c38bcd3bb1f02d17754aa75a392bea11ac704bb4
cakmaak <yusuf612844@gmail.com>
2026-02-06 15:56:43 +0300

    upd admınpage

--- src/main/java/com/Alizone/Conroller/ProductController.java
+++ src/main/java/com/Alizone/Conroller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +53,7 @@ public class ProductController implements IProductController {
 	}
 	
 	@PreAuthorize("hasRole('ADMIN')")
-	@PostMapping("/setname/{id}")
+	@PutMapping("/setname/{id}")
 	@Override
 	public String setnamebyProduct(@PathVariable Long id, @RequestBody String name) {
 		
